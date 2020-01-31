e49f37d57ae90662cf77a1fc745ddacb61176d0d
Miguel Paysan <miguelpaysan@gmail.com>
2020-01-30 23:22:56 -0800

    add basic table view of firestore data. created Post data model with Codeable to parse json directly into object

--- spotcheck-ios/DataModels/Post.swift
+++ spotcheck-ios/DataModels/Post.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct Post: Codable {
+    static let POSTS_COLLECTION_NAME = "posts"
+    let postId: String
+    
+    var authorId = "",
+    authorName = "",
+    createdAt = "",
+    updatedAt = "",
+    question = ""
+    
+}
+
+protocol PostsService {}
+
+//Refer to https://medium.com/@foffer/easily-parse-firebase-object-in-swift-bfe151eada41
+extension PostsService {
+    
+    var firestore: Firestore { return Firestore.firestore() }
+    
+    func fetchPost(withId id: String, completion: @escaping (Post?) -> Post?) {
+        
+        let docRef = firestore.collection(Post.POSTS_COLLECTION_NAME).document(id)
+        docRef.getDocument { docSnapshot, error in
+            guard error == nil, let doc = docSnapshot, doc.exists == true else {
+                print(error)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "yyyy-MM-dd kk:mm:ss Z"
+            decoder.dateDecodingStrategy = .formatted(dateFormatter)
+            
+            //Make a mutable copy of the NSDictionary
+            var dict = doc.data()
+            
+            for (key, value) in dict! {
+                //We need to check if the value is a Timestamp and parse it as a string, since you can't serialize a Timestamp. This might be true for other types you have serverside.
+                if let value = value as? Timestamp {
+                    dict?[key] = dateFormatter.string(from: value.dateValue())
+                }
+            }
+            
+            //Serialize the Dictionary into a JSON Data representation then decode it using the Decoder()
+            if let data = try? JSONSerialization.data(withJSONObject: dict!, options: []) {
+                let post = try? decoder.decode(Post.self, from: data)
+                completion(post)
+            }
+        }
+    }
+}
+
+
+
--- spotcheck-ios/FeedViewController.swift
+++ spotcheck-ios/FeedViewController.swift
@@ -7,11 +7,76 @@
 //
 
 import Foundation
-import Firebase
 
-class FeedViewController: UIViewController {
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class FeedViewController: UITableViewController, PostsService {
+    
+    var db: Firestore!
+    var posts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("@FeedViewController")
+        
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
+        
+        tableView.rowHeight = 80
+        tableView.allowsMultipleSelectionDuringEditing = false
+        
+        db.collection("posts").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents{
+                                        
+                    print("\(document.documentID)")
+                    print("\(document.documentID) => \(document.data())")
+                    
+                    //closure success will run this, saving the context of all the external variables when we pass it
+                    var createPost = { (argPost: Post?) -> Post? in
+                        print(argPost!.question , " by:" , argPost!.authorName)
+                        if let post = argPost {
+                            self.posts.append(post)
+                            self.tableView.reloadData()
+                        }
+                        return argPost
+                    }
+                    self.fetchPost(withId: document.documentID, completion: createPost)
+                    
+                }
+            }
+            
+        }
+    }
+    
+    
+    // MARK: UITableView Delegate methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "----"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    // MARK: Populating table cells
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = posts[indexPath.row].question
+        cell.detailTextLabel?.text = posts[indexPath.row].authorName
+        
+        return cell
     }
 }
