d4b80d6fe6429f7973b5ad420ceaf95129cbf9db
uioporqwerty <nitish.sachar@protonmail.com>
2020-02-01 23:35:12 -0800

    improvement(login-ui): add theme support with fonts, colors, and finalize login layout

--- spotcheck-ios/AuthOptionsViewController.swift
+++ spotcheck-ios/AuthOptionsViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 import Firebase
 import FirebaseUI
 import MaterialComponents
+import MaterialComponents.MaterialButtons_Theming
 
 class AuthOptionsViewController: UIViewController {
 
+    @IBOutlet weak var SignUpButton: MDCButton!
+    @IBOutlet weak var LogInButton: MDCButton!
+    
+    @IBOutlet weak var SpotcheckHeadline: UILabel!
+    @IBOutlet weak var SpotcheckSubtitle: UILabel!
+    @IBOutlet weak var ConnectWithGoogleButton: MDCButton!
+    @IBOutlet weak var ConnectWithFacebookButton: MDCButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        SignUpButton.applyContainedTheme(withScheme: ApplicationScheme.instance.containerScheme)
+        LogInButton.applyContainedTheme(withScheme: ApplicationScheme.instance.containerScheme)
+        SpotcheckHeadline.font = ApplicationScheme.instance.containerScheme.typographyScheme.headline4
+        SpotcheckSubtitle.font = ApplicationScheme.instance.containerScheme.typographyScheme.subtitle1
+        ConnectWithGoogleButton.applyContainedTheme(withScheme: ApplicationScheme.instance.containerScheme)
+        ConnectWithFacebookButton.applyContainedTheme(withScheme: ApplicationScheme.instance.containerScheme)
+        
     }
 
     @objc private func authenticationFinished() {
