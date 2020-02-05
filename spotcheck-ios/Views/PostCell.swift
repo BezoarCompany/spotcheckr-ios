3df5c0b764729a44785957cf9fd9ce6a94ea7608
Miguel Paysan <miguelpaysan@gmail.com>
2020-02-05 00:53:11 -0800

    getting a decently aligned Post Cell

---
+++ spotcheck-ios/Views/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  spotcheck-ios
+//
+//  Created by Miguel Paysan on 2/4/20.
+//  Copyright © 2020 Miguel Paysan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
