3edda0272cd1cde244ce330d0f5776bee76b37ea
uioporqwerty <nitish.sachar@protonmail.com>
2020-02-11 16:20:50 -0800

    feat: top information style

--- spotcheck-ios/ProfileViewController.swift
+++ spotcheck-ios/ProfileViewController.swift
@@ -3,6 +3,10 @@ import Firebase
 
 class ProfileViewController: UIViewController {
     @IBOutlet weak var logoutButton: UIBarButtonItem!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var certificationsHeadingLabel: UILabel!
+    @IBOutlet weak var certificationsLabel: UILabel!
+    @IBOutlet weak var occupationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,10 +15,14 @@ class ProfileViewController: UIViewController {
     }
     
     private func applyStyles() {
-        logoutButton.setTitleTextAttributes(
-            [
-            NSAttributedString.Key.font: ApplicationScheme.instance.containerScheme.typographyScheme.button,
-            NSAttributedString.Key.foregroundColor: ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor], for: .normal)
+        nameLabel.font = ApplicationScheme.instance.containerScheme.typographyScheme.body1
+        nameLabel.textColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        certificationsLabel.font = ApplicationScheme.instance.containerScheme.typographyScheme.body1
+        certificationsLabel.textColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        certificationsHeadingLabel.font = ApplicationScheme.instance.containerScheme.typographyScheme.body1
+        certificationsHeadingLabel.textColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        occupationLabel.font = ApplicationScheme.instance.containerScheme.typographyScheme.body1
+        occupationLabel.textColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
     }
     
     @IBAction func logoutTapped(_ sender: Any) {
