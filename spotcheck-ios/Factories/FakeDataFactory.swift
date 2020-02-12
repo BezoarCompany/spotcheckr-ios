1864e13139b857561bd42489886f5a3ce4ec373e
uioporqwerty <nitish.sachar@protonmail.com>
2020-02-11 19:45:38 -0800

    feat: add fake exercise post data generator

--- spotcheck-ios/Factories/FakeDataFactory.swift
+++ spotcheck-ios/Factories/FakeDataFactory.swift
@@ -4,7 +4,17 @@ import Fakery
 class FakeDataFactory {
     private static let faker = Faker()
     private static let nasmSpecializations = ["PES", "CES", "FNS", "WLS", "YES", "WFS", "MMAS", "GFS", "SFS"]
-    
+    private static let exercises = [Exercise(name: "Deadlift"),
+                                    Exercise(name:"Squat"),
+                                    Exercise(name: "Bench Press"),
+                                    Exercise(name: "Sit-up"),
+                                    Exercise(name: "Push-up"),
+                                    Exercise(name: "Running"),
+                                    Exercise(name: "Hiking"),
+                                    Exercise(name: "Sex"),
+                                    Exercise(name: "Incline Bench Press"),
+                                    Exercise(name: "Decline Bench Press")
+                                    ]
     static func GetUsers(count: Int) -> [User] {
         var users = [User]()
         
@@ -23,6 +33,60 @@ class FakeDataFactory {
         }
         
         return trainers
+    }
+    
+    static func GetExercisePosts(count: Int) -> [ExercisePost] {
+        var exercisePosts = [ExercisePost]()
+        
+        for _ in 1...count {
+            let dateCreated = faker.date.between(Date("2000-01-01"), Date("2020-01-01"))
+            var likes = [Like]()
+            for _ in 0...Int.random(in: 0..<1000) {
+                likes.append(Like(LikedBy: createFakeUser()))
+            }
+            
+            var answers = [Answer]()
+            for _ in 0...Int.random(in: 0..<100) {
+                var media = [Media]()
+                for _ in 0...Int.random(in: 0..<2){
+                    media.append(Media(url: URL(string:faker.internet.image())))
+                }
+                
+                answers.append(Answer(createdBy: createFakeUser(),
+                                      modifiedDate: faker.date.between(dateCreated, Date("2020-01-01")),
+                                      text: faker.lorem.paragraphs(),
+                                      media: media,
+                                      upvotes: faker.number.randomInt(min: 1, max: 1000),
+                                      downvotes: faker.number.randomInt(min: 1, max: 1000)))
+            }
+            
+            var postMedia = [Media]()
+            for _ in 0...Int.random(in: 0..<2) {
+                postMedia.append(Media(url: URL(string:faker.internet.image())))
+            }
+            
+            var postExercises = [Exercise]() //Can have duplicates but, whatever
+            for _ in 0...Int.random(in: 0..<3) {
+                postExercises.append(exercises[Int.random(in: 0..<exercises.count)])
+            }
+            
+            exercisePosts.append(
+                ExercisePost(id: "\(faker.number.increasingUniqueId())",
+                            title: faker.lorem.sentence(),
+                            description: faker.lorem.paragraphs(),
+                            createdBy: GetUsers(count: 1)[0],
+                            dateCreated: dateCreated,
+                            dateModified: faker.date.between(dateCreated, Date("2020-01-01")),
+                            metrics: Metrics(views: faker.number.randomInt(min: 1, max: 100000),
+                                             likes: likes,
+                                             upvotes: faker.number.randomInt(min: 1, max: 10000),
+                                             downvotes: faker.number.randomInt(min: 1, max: 10000)),
+                            answers: answers,
+                            media: postMedia,
+                            exercises: postExercises))
+        }
+        
+        return exercisePosts
     }
     
     private static func createFakeUser() -> User {
