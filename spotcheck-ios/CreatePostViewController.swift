edd58f4022386f40e8ecbe65312178ba8d695ce9
Miguel Paysan <miguelpaysan@gmail.com>
2020-02-13 00:45:33 -0800

    added dropdown menu with iOSDropDown Cocoapod. TODO dummy data

--- spotcheck-ios/CreatePostViewController.swift
+++ spotcheck-ios/CreatePostViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
+import iOSDropDown //https://github.com/jriosdev/iOSDropDown
 
 class CreatePostViewController: UIViewController {
 
+    @IBOutlet weak var workoutTypeDropDown: DropDown!
+    
     static func create() -> CreatePostViewController  {
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         let createPostViewController = storyboard.instantiateViewController(withIdentifier: K.Storyboard.CreatePostViewControllerId) as! CreatePostViewController
@@ -12,6 +15,16 @@ class CreatePostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("CreatePostViewController")
+        
+        workoutTypeDropDown.selectedRowColor = .magenta
+        
+        //TODO load from Firebase
+        workoutTypeDropDown.optionArray = ["Bench TEST", "Cardio TEST", "Games Test"]
+        
+        workoutTypeDropDown.didSelect{
+            (selectedText, index, id) in
+            print("\(selectedText) @ index: \(index)")
+        }
         
     }
     
