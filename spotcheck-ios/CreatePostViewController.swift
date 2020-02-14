91333a2b3479f2c04ee13ff75ae0f893c0431c48
Miguel Paysan <miguelpaysan@gmail.com>
2020-02-13 22:55:04 -0800

    added text placeholders for Subject and Body Text views

--- spotcheck-ios/CreatePostViewController.swift
+++ spotcheck-ios/CreatePostViewController.swift
@@ -4,7 +4,15 @@ import iOSDropDown //https://github.com/jriosdev/iOSDropDown
 class CreatePostViewController: UIViewController {
 
     @IBOutlet weak var workoutTypeDropDown: DropDown!
-    
+    @IBOutlet weak var subjectTextView: UITextView!
+    @IBOutlet weak var postBodyTextView: UITextView!
+    @IBAction func cancelPost(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    static let SUBJECT_TEXT_PLACEHOLDER = "Subject"
+    static let POST_BODY_TEXT_PLACEHOLDER = "Write your question"
+
     static func create() -> CreatePostViewController  {
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         let createPostViewController = storyboard.instantiateViewController(withIdentifier: K.Storyboard.CreatePostViewControllerId) as! CreatePostViewController
@@ -16,6 +24,13 @@ class CreatePostViewController: UIViewController {
         super.viewDidLoad()
         print("CreatePostViewController")
         
+        initDropDown()
+        initTextViewPlaceholders()
+    }    
+}
+
+extension CreatePostViewController {
+    func initDropDown() {
         workoutTypeDropDown.selectedRowColor = .magenta
         
         //TODO load from Firebase
@@ -25,15 +40,38 @@ class CreatePostViewController: UIViewController {
             (selectedText, index, id) in
             print("\(selectedText) @ index: \(index)")
         }
-        
     }
     
-    @IBAction func cancelPost(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    func initTextViewPlaceholders() {
+        subjectTextView.delegate = self
+        postBodyTextView.delegate = self
+        
+        subjectTextView.text = CreatePostViewController.SUBJECT_TEXT_PLACEHOLDER
+        subjectTextView.textColor = UIColor.lightGray
+        
+        postBodyTextView.text = CreatePostViewController.POST_BODY_TEXT_PLACEHOLDER
+        postBodyTextView.textColor = UIColor.lightGray
     }
-
 }
 
-extension CreatePostViewController {
+//Resetting textview's text to gray and placeholder value if empty,
+//or back to black if non-empty (key press event triggered)
+extension CreatePostViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
     
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            if textView == subjectTextView {
+                textView.text = CreatePostViewController.SUBJECT_TEXT_PLACEHOLDER
+            } else {
+                textView.text = CreatePostViewController.POST_BODY_TEXT_PLACEHOLDER
+            }
+            textView.textColor = UIColor.lightGray
+        }
+    }
 }
