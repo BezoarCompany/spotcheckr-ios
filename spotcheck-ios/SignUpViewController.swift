d05fa885c9ecbfd0128fdd6861b9713a94bda2e6
uioporqwerty <nitish.sachar@protonmail.com>
2020-02-23 21:09:45 -0800

    fix: sign up view incorrect style of input fields

--- spotcheck-ios/SignUpViewController.swift
+++ spotcheck-ios/SignUpViewController.swift
@@ -54,8 +54,18 @@ class SignUpViewController: UIViewController, UITextFieldDelegate, ValidationDel
     required init?(coder aDecoder: NSCoder) {
         emailAddressTextFieldController = MDCTextInputControllerOutlined(textInput: emailAddressTextField)
         emailAddressTextFieldController.applyTheme(withScheme: ApplicationScheme.instance.containerScheme)
+        emailAddressTextFieldController.normalColor  = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        emailAddressTextFieldController.activeColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        emailAddressTextFieldController.inlinePlaceholderColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        emailAddressTextFieldController.floatingPlaceholderNormalColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        emailAddressTextFieldController.floatingPlaceholderActiveColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
         passwordTextFieldController = MDCTextInputControllerOutlined(textInput: passwordTextField)
         passwordTextFieldController.applyTheme(withScheme: ApplicationScheme.instance.containerScheme)
+        passwordTextFieldController.normalColor  = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        passwordTextFieldController.activeColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        passwordTextFieldController.inlinePlaceholderColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        passwordTextFieldController.floatingPlaceholderNormalColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
+        passwordTextFieldController.floatingPlaceholderActiveColor = ApplicationScheme.instance.containerScheme.colorScheme.onPrimaryColor
         passwordTextFieldController.helperText = "Password must be at least 8 characters long"
         validator = Validator()
         super.init(coder: aDecoder)
