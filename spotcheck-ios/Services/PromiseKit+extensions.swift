32547b362e33a2ebe154c35685d900d991d705da
Miguel Paysan <miguelpaysan@gmail.com>
2020-03-21 03:35:45 -0700

    Create Promise extension method for Chaining/executing a series of ClosuresToPromises

---
+++ spotcheck-ios/Services/PromiseKit+extensions.swift
@@ -0,0 +1,9 @@
+//
+//  PromiseKit+extensions.swift
+//  spotcheck-ios
+//
+//  Created by Miguel Paysan on 3/21/20.
+//  Copyright © 2020 Miguel Paysan. All rights reserved.
+//
+
+import Foundation
