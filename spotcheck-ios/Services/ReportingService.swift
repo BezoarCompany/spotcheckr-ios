9a31f19485b9eb339803e0c5867a16cb537bb3bf
uioporqwerty <nitish.sachar@protonmail.com>
2020-04-02 10:25:08 -0700

    perf: add caching for getReportOptions

--- spotcheck-ios/Services/ReportingService.swift
+++ spotcheck-ios/Services/ReportingService.swift
@@ -3,9 +3,14 @@ import FirebaseFirestore
 
 class ReportingService: ReportingProtocol {
     private let reportTypesCollection = "report-types"
+     private let cache = Cache<String,Any>()
     
     func getReportOptions() -> Promise<[Report]> {
         return Promise { promise in
+            if let reportTypes = cache["report-types"] as? [Report] {
+                return promise.fulfill(reportTypes)
+            }
+            
             let reportTypesCollectionRef = Firestore.firestore().collection(reportTypesCollection)
             reportTypesCollectionRef.getDocuments { (snapshot, error) in
                 if let error = error {
@@ -16,7 +21,7 @@ class ReportingService: ReportingProtocol {
                 for document in snapshot!.documents {
                     reportTypes.append(FirebaseToDomainMapper.mapReport(id: document.documentID, data: document.data()))
                 }
-                
+                self.cache.insert(reportTypes, forKey: "report-types")
                 return promise.fulfill(reportTypes)
             }
         }
