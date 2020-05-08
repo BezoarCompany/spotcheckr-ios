62912c2cafb72ff4766986b87f24f1a0a668b2c1
Nitish Sachar <uioporqwerty@users.noreply.github.com>
2020-05-07 23:43:09 -0700

    Feature/refactor post detail iglistkit (#146)
    * refactor: Post Detail use iglistkit for post
    
    * refactor: use view model and extension of domain objects
    
    * refactor: answers section to iglistkit
    
    * fix: add missing answers refactor work
    
    * fix: label and activity indicator does not center within sections

---
+++ spotcheck-ios/Models/Extensions/IGListKit/ExercisePostExtension.swift
@@ -0,0 +1,23 @@
+import IGListKit
+
+extension ExercisePost: ListDiffable {
+    func diffIdentifier() -> NSObjectProtocol {
+        return self.id!.value as NSObjectProtocol
+    }
+    
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        guard let post = (object as? ExercisePost) else {
+            return false
+        }
+        
+        if self.dateModified != post.dateModified
+            || self.title != post.title
+            || self.description != post.description
+            || self.imagePath != post.imagePath
+            || self.videoPath != post.videoPath {
+            return false
+        }
+        
+        return true
+    }
+}
